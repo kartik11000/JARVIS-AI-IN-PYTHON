406855d6b2447557aa2de5f95c9fd9eb1096f2ea
Kartik Rajput <kr5815720@gmail.com>
2025-01-04 12:53:44 +0530

    Add files via upload
    control volume

---
+++ keyboard.py
@@ -0,0 +1,17 @@
+import controller
+import keyboard
+from time import sleep
+import     pyautogui
+
+# keyboard=controller()
+def volumeup(query):
+    for i  in range(10):
+        pyautogui.press("volumeup")
+        
+        
+        
+def volumedown( query):
+    for i in range(10):
+        pyautogui.press("volumedown")
+        
+        
